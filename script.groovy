66d7c92569f8c8ca6d2add5d7185080710f4e743
Ryan Hoofard <42755382+rhoofard@users.noreply.github.com>
2024-06-04 14:47:34 -0700

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,1 +1,9 @@
-println "Hello World!"
+@Grab(group='org.jsoup', module='jsoup', version='1.13.1')
+import org.jsoup.Jsoup
+
+// Use Jsoup to fetch and parse HTML from a website
+def url = 'https://en.wikipedia.org/wiki/Groovy_(programming_language)'
+def doc = Jsoup.connect(url).get()
+
+// Print the title of the Wikipedia page
+println "Title: ${doc.title()}"
